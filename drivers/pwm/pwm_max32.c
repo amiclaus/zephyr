067346f784eb669f148c79d37568cb34389781e8
Sadik Ozer <sadik.ozer@analog.com>
2023-09-25 11:47:25 +0300

    drivers: pwm: Enable timer peripheral clock
    Update device tree to includes timer clock index,
    Enable timer peripheral clock incase of PWM enabled.
    Quick review on PWM driver, there are too many thing need
    to be applied her, it is in the list.
    
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>

--- drivers/pwm/pwm_max32.c
+++ drivers/pwm/pwm_max32.c
@@ -8,6 +8,7 @@
 
 #include <errno.h>
 
+#include <zephyr/device.h>
 #include <zephyr/drivers/clock_control.h>
 #include <zephyr/drivers/clock_control/adi_max32_clock_control.h>
 #include <zephyr/drivers/pwm.h>
@@ -26,36 +27,42 @@ LOG_MODULE_REGISTER(pwm_max32, CONFIG_PWM_LOG_LEVEL);
 
 /** PWM configuration. */
 struct max32_pwm_config {
-	/** Timer register. */
 	mxc_tmr_regs_t *regs;
-	wrap_mxc_tmr_cfg_t *cfg;
+	const struct device *clock;
+	struct max32_perclk perclk;
 };
 
 /** PWM data. */
 struct max32_pwm_data {
-	/** Timer clock (Hz). */
-	uint32_t tim_clk;
+	uint32_t period_cycles;
 };
 
-static int pwm_max32_set_cycles(const struct device *dev, uint32_t channel, uint32_t period_cycles,
-				uint32_t pulse_cycles, pwm_flags_t flags)
+static int api_set_cycles(const struct device *dev, uint32_t channel, uint32_t period_cycles,
+			  uint32_t pulse_cycles, pwm_flags_t flags)
 {
+	int ret = 0;
 	mxc_tmr_regs_t *regs = TMR_CFG(dev)->regs;
-	wrap_mxc_tmr_cfg_t cfg;
-	int ret;
+	const struct max32_pwm_config *const cfg = dev->config;
+	wrap_mxc_tmr_cfg_t pwm_cfg;
 
-	cfg.pres = TMR_PRES_1;
-	cfg.mode = TMR_MODE_PWM;
-	cfg.cmp_cnt = period_cycles;
-	cfg.pol = 1;
-	
+	pwm_cfg.pres = TMR_PRES_1;
+	pwm_cfg.mode = TMR_MODE_PWM;
+	pwm_cfg.cmp_cnt = period_cycles;
+	pwm_cfg.pol = 1;
+
 #if defined(CONFIG_SOC_MAX32690) || (CONFIG_SOC_MAX32655)
-	cfg.clock = MXC_TMR_APB_CLK;
-#endif 
+	pwm_cfg.clock = MXC_TMR_APB_CLK;
+#endif
 
 	MXC_TMR_Shutdown(regs);
 
-	Wrap_MXC_TMR_Init(regs, &cfg);
+	/* enable clock */
+	ret = clock_control_on(cfg->clock, (clock_control_subsys_t) &(cfg->perclk));
+	if (ret) {
+		return ret;
+	}
+
+	Wrap_MXC_TMR_Init(regs, &pwm_cfg);
 
 	ret = MXC_TMR_SetPWM(regs, pulse_cycles);
 	if (ret != E_NO_ERROR) {
@@ -67,8 +74,7 @@ static int pwm_max32_set_cycles(const struct device *dev, uint32_t channel, uint
 	return 0;
 }
 
-static int pwm_max32_get_cycles_per_sec(const struct device *dev, uint32_t channel,
-					uint64_t *cycles)
+static int api_get_cycles_per_sec(const struct device *dev, uint32_t channel, uint64_t *cycles)
 {
 	*cycles = (uint64_t)(PeripheralClock / (TMR_PRES_1 + 1));
 
@@ -76,44 +82,29 @@ static int pwm_max32_get_cycles_per_sec(const struct device *dev, uint32_t chann
 }
 
 static const struct pwm_driver_api pwm_max32_driver_api = {
-	.set_cycles = pwm_max32_set_cycles,
-	.get_cycles_per_sec = pwm_max32_get_cycles_per_sec,
+	.set_cycles = api_set_cycles,
+	.get_cycles_per_sec = api_get_cycles_per_sec,
 };
 
 static int pwm_max32_init(const struct device *dev)
 {
-	mxc_tmr_regs_t *regs = TMR_CFG(dev)->regs;
-	wrap_mxc_tmr_cfg_t cfg;
-	int ret;
+	int ret = 0;
 
-	MXC_TMR_Shutdown(regs);
-
-	cfg.pres = TMR_PRES_1;
-	cfg.mode = TMR_MODE_PWM;
-	cfg.cmp_cnt = PeripheralClock;
-	cfg.pol = 1;
-
-	Wrap_MXC_TMR_Init(regs, &cfg);
-
-	ret = MXC_TMR_SetPWM(regs, 0);
-	if (ret != E_NO_ERROR) {
-		return ret;
-	}
-
-	MXC_TMR_Start(regs);
-
-	return 0;
+	return ret;
 }
 
-#define PWM_MAX32_DEFINE(i)                                                                        \
-	static struct max32_pwm_data max32_pwm_data_##i;                                           \
+#define PWM_MAX32_DEFINE(_num)                                                                     \
+	static struct max32_pwm_data max32_pwm_data_##_num;                                        \
                                                                                                    \
-	static const struct max32_pwm_config max32_pwm_config_##i = {                              \
-		.regs = (mxc_tmr_regs_t *)DT_REG_ADDR(DT_INST_PARENT(i)),                          \
+	static const struct max32_pwm_config max32_pwm_config_##_num = {                           \
+		.regs = (mxc_tmr_regs_t *)DT_REG_ADDR(DT_INST_PARENT(_num)),                       \
+		.clock = DEVICE_DT_GET(DT_CLOCKS_CTLR(DT_INST_PARENT(_num))),                      \
+		.perclk.bus = DT_CLOCKS_CELL(DT_INST_PARENT(_num), offset),                        \
+		.perclk.bit = DT_CLOCKS_CELL(DT_INST_PARENT(_num), bit),                           \
 	};                                                                                         \
                                                                                                    \
-	DEVICE_DT_INST_DEFINE(i, &pwm_max32_init, NULL, &max32_pwm_data_##i,                       \
-			      &max32_pwm_config_##i, POST_KERNEL, CONFIG_PWM_INIT_PRIORITY,        \
+	DEVICE_DT_INST_DEFINE(_num, &pwm_max32_init, NULL, &max32_pwm_data_##_num,                 \
+			      &max32_pwm_config_##_num, POST_KERNEL, CONFIG_PWM_INIT_PRIORITY,     \
 			      &pwm_max32_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(PWM_MAX32_DEFINE)
