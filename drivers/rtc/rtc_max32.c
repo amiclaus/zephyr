4f39146f1cc225380fe7d8cdc4fec0cbd79c75df
Furkan Akkiz <hasanfurkan.akkiz@analog.com>
2023-10-25 09:25:05 +0300

    driver: rtc: Update RTC driver
    - Added alarm interface support for second, minute, hour and weekday.
    - Added update interface support.
    - Added calibration interface support.
    
    Signed-off-by: Furkan Akkiz <hasanfurkan.akkiz@analog.com>

--- drivers/rtc/rtc_max32.c
+++ drivers/rtc/rtc_max32.c
@@ -20,48 +20,63 @@
 #include <rtc.h>
 #include <time.h>
 
-#define RTC_CFG(dev)  ((struct max32_rtc_config *)((dev)->config))
-#define RTC_DATA(dev) ((struct max32_rtc_data *)((dev)->data))
-
 #define MSEC_TO_NSEC(x) ((x)*1000000)
 
 /* Converts a time in milleseconds to the equivalent RSSA register value. */
 #define NSEC_TO_MSEC(x) (x / 1000000)
 #define NSEC_TO_RSSA(x) (0 - ((NSEC_TO_MSEC(x) * 4096) / 1000))
 
+#define MAX_PPB 127
+#define MIN_PPB -127
+
+#define SECS_PER_MIN   60
+#define SECS_PER_HOUR  (60 * SECS_PER_MIN)
+#define SECS_PER_DAY   (24 * SECS_PER_HOUR)
+#define SECS_PER_WEEK  (7 * SECS_PER_DAY)
+#define SUBSEC_PER_SEC 4096
+
+#define MAX_ALARM_SEC SECS_PER_WEEK - 1
+#define RTC_ALARM_MASK                                                                             \
+	(RTC_ALARM_TIME_MASK_SECOND | RTC_ALARM_TIME_MASK_MINUTE | RTC_ALARM_TIME_MASK_HOUR |      \
+	 RTC_ALARM_TIME_MASK_WEEKDAY)
+
 struct max32_rtc_data {
 	struct k_spinlock lock;
 	uint16_t alarms_count;
 	uint16_t mask;
+#ifdef CONFIG_RTC_ALARM
 	bool alarm_pending;
-	rtc_alarm_callback cb;
-	void *cb_data;
-	struct rtc_time alarm_time;
+	rtc_alarm_callback alarm_cb;
+	void *alarm_cb_data;
+	time_t alarm_sec;
+#endif /* CONFIG_RTC_ALARM */
+#ifdef CONFIG_RTC_UPDATE
+	rtc_update_callback update_cb;
+	void *update_cb_data;
+#endif /* CONFIG_RTC_UPDATE */
 };
 
 struct max32_rtc_config {
 	mxc_rtc_regs_t *regs;
-#ifdef CONFIG_RTC_ALARM
 	void (*irq_func)(void);
-#endif /* CONFIG_RTC_ALARM */
 };
 
 static inline time_t convert_to_second(const struct rtc_time *timeptr)
 {
-	struct tm stm;
+	struct tm s_tm;
 	time_t sec;
 
-	stm.tm_sec = timeptr->tm_sec;
-	stm.tm_min = timeptr->tm_min;
-	stm.tm_hour = timeptr->tm_hour;
-	stm.tm_mday = timeptr->tm_mday;
-	stm.tm_mon = timeptr->tm_mon;
-	stm.tm_year = timeptr->tm_year;
-	stm.tm_wday = timeptr->tm_wday;
-	stm.tm_yday = timeptr->tm_yday;
-	stm.tm_isdst = timeptr->tm_isdst;
+	s_tm.tm_sec = timeptr->tm_sec;
+	s_tm.tm_min = timeptr->tm_min;
+	s_tm.tm_hour = timeptr->tm_hour;
+	s_tm.tm_mday = timeptr->tm_mday;
+	s_tm.tm_mon = timeptr->tm_mon;
+	s_tm.tm_year = timeptr->tm_year;
+	s_tm.tm_wday = timeptr->tm_wday;
+	s_tm.tm_yday = timeptr->tm_yday;
+	s_tm.tm_isdst = timeptr->tm_isdst;
 
-	sec = timeutil_timegm(&stm); /* Convert rtc_time to seconds*/
+	sec = timeutil_timegm(&s_tm); /* Convert rtc_time to seconds*/
 
 	return sec;
 }
@@ -69,41 +84,99 @@ static inline time_t convert_to_second(const struct rtc_time *timeptr)
 static inline void convert_to_rtc_time(uint32_t sec, uint32_t subsec, struct rtc_time *timeptr)
 {
 	time_t tm_t;
-	struct tm stm;
+	struct tm s_tm;
 
 	tm_t = sec;
-	gmtime_r(&tm_t, &stm);
+	gmtime_r(&tm_t, &s_tm);
 
-	timeptr->tm_sec = stm.tm_sec;
-	timeptr->tm_min = stm.tm_min;
-	timeptr->tm_hour = stm.tm_hour;
-	timeptr->tm_mday = stm.tm_mday;
-	timeptr->tm_mon = stm.tm_mon;
-	timeptr->tm_year = stm.tm_year;
-	timeptr->tm_wday = stm.tm_wday;
-	timeptr->tm_yday = stm.tm_yday;
-	/* timeptr->tm_isdst = stm.tm_isdst; */
+	timeptr->tm_sec = s_tm.tm_sec;
+	timeptr->tm_min = s_tm.tm_min;
+	timeptr->tm_hour = s_tm.tm_hour;
+	timeptr->tm_mday = s_tm.tm_mday;
+	timeptr->tm_mon = s_tm.tm_mon;
+	timeptr->tm_year = s_tm.tm_year;
+	timeptr->tm_wday = s_tm.tm_wday;
+	timeptr->tm_yday = s_tm.tm_yday;
 	timeptr->tm_isdst = -1;
+	timeptr->tm_nsec = 0;
 	/* add subsec too */
-	timeptr->tm_nsec = MSEC_TO_NSEC(subsec / 4.096); /* 4096 count equal to 1sec */
+	if (subsec >= (SUBSEC_PER_SEC / 2)) {
+		timeptr->tm_sec += 1;
+	}
 }
 
 static int api_set_time(const struct device *dev, const struct rtc_time *timeptr)
 {
-	ARG_UNUSED(dev);
+	time_t sec = 0;
+
 	if (timeptr == NULL) {
 		return -EINVAL;
 	}
-
-	time_t sec;
 
 	sec = convert_to_second(timeptr);
 	if (sec == -1) {
 		return -1;
 	}
 
-	MXC_RTC_Init((uint32_t)sec, NSEC_TO_RSSA(timeptr->tm_nsec));
-	MXC_RTC_Start();
+	while (MXC_RTC_Init((uint32_t)sec, NSEC_TO_RSSA(timeptr->tm_nsec)) == E_BUSY) {
+		;
+	}
+
+#ifdef CONFIG_RTC_ALARM
+	struct max32_rtc_data *data = dev->data;
+	uint32_t alarm_time = 0;
+
+	if (data->alarm_sec != -1) {
+		while (MXC_RTC_DisableInt(MXC_RTC_INT_EN_LONG) == E_BUSY) {
+			;
+		}
+
+		if (data->alarm_sec < SECS_PER_MIN) { /* less than a minute */
+			if ((sec % SECS_PER_MIN) <= data->alarm_sec) {
+				alarm_time = sec - (sec % SECS_PER_MIN) + (data->alarm_sec);
+			} else {
+				alarm_time = sec - (sec % SECS_PER_MIN) + (data->alarm_sec) +
+					     SECS_PER_MIN;
+			}
+		} else if (data->alarm_sec < SECS_PER_HOUR) { /* less than a hour */
+			if ((sec % SECS_PER_HOUR) <= data->alarm_sec) {
+				alarm_time = sec - (sec % SECS_PER_HOUR) + (data->alarm_sec);
+			} else {
+				alarm_time = sec - (sec % SECS_PER_HOUR) + (data->alarm_sec) +
+					     SECS_PER_HOUR;
+			}
+		} else if (data->alarm_sec < SECS_PER_DAY) { /* less than a day */
+			if ((sec % SECS_PER_DAY) <= data->alarm_sec) {
+				alarm_time = sec - (sec % SECS_PER_DAY) + (data->alarm_sec);
+			} else {
+				alarm_time = sec - (sec % SECS_PER_DAY) + (data->alarm_sec) +
+					     SECS_PER_DAY;
+			}
+		} else if (data->alarm_sec < SECS_PER_WEEK) { /* less than a week */
+			uint8_t wday_alarm = (data->alarm_sec) / SECS_PER_DAY;
+			if (timeptr->tm_wday <= wday_alarm) {
+				alarm_time =
+					sec -
+					((timeptr->tm_wday * SECS_PER_DAY) + (sec % SECS_PER_DAY)) +
+					(data->alarm_sec);
+			} else {
+				alarm_time =
+					sec -
+					((timeptr->tm_wday * SECS_PER_DAY) + (sec % SECS_PER_DAY)) +
+					(data->alarm_sec) + SECS_PER_WEEK;
+			}
+		}
+
+		MXC_RTC_SetTimeofdayAlarm(alarm_time);
+		while (MXC_RTC_EnableInt(MXC_RTC_INT_EN_LONG) == E_BUSY) {
+			;
+		}
+	}
+#endif /* CONFIG_RTC_ALARM */
+
+	while (MXC_RTC_Start() == E_BUSY) {
+		;
+	}
 
 	return 0;
 }
@@ -116,22 +189,19 @@ static int api_get_time(const struct device *dev, struct rtc_time *timeptr)
 	while (MXC_RTC_GetTime(&sec, &subsec) != E_NO_ERROR) {
 		;
 	}
-
 	convert_to_rtc_time(sec, subsec, timeptr);
-
 	return 0;
 }
 
 #ifdef CONFIG_RTC_ALARM
 static int api_alarm_get_supported_fields(const struct device *dev, uint16_t id, uint16_t *mask)
 {
-	if (RTC_DATA(dev)->alarms_count <= id) {
+	struct max32_rtc_data *data = dev->data;
+	if (data->alarms_count <= id) {
 		return -EINVAL;
 	}
 
-	(*mask) = (RTC_ALARM_TIME_MASK_SECOND | RTC_ALARM_TIME_MASK_MINUTE |
-		   RTC_ALARM_TIME_MASK_HOUR | RTC_ALARM_TIME_MASK_MONTHDAY |
-		   RTC_ALARM_TIME_MASK_MONTH);
+	(*mask) = RTC_ALARM_MASK;
 
 	return 0;
 }
@@ -139,45 +209,51 @@ static int api_alarm_get_supported_fields(const struct device *dev, uint16_t id,
 static int api_alarm_set_time(const struct device *dev, uint16_t id, uint16_t mask,
 			      const struct rtc_time *timeptr)
 {
-	struct max32_rtc_data *const dev_data = dev->data;
-	uint32_t crr_sec, crr_subsec;
+	struct max32_rtc_data *const data = dev->data;
 
-	if (dev_data->alarms_count <= id) {
+	if (data->alarms_count <= id) {
 		return -EINVAL;
 	}
 
-	if (timeptr == NULL) {
+	if (mask & ~RTC_ALARM_MASK) {
 		return -EINVAL;
 	}
 
 	/* means disable alarm */
 	if (mask == 0) {
+		data->mask = 0;
+		data->alarm_sec = -1;
 		while (MXC_RTC_DisableInt(MXC_RTC_INT_EN_LONG) == E_BUSY) {
 			;
 		}
 		return 0;
 	}
 
-	dev_data->mask = mask;
-	dev_data->alarm_time = *timeptr;
-
-	while (MXC_RTC_GetTime(&crr_sec, &crr_subsec) != E_NO_ERROR) {
-		;
+	if (timeptr == NULL) {
+		return -EINVAL;
 	}
 
-	time_t alarm_sec = convert_to_second(timeptr);
-
-	if ((uint32_t)alarm_sec <= crr_sec) {
-		return EINVAL;
+	// Calculating sec number from timeptr
+	time_t alarm_sec = 0;
+	if (mask & RTC_ALARM_TIME_MASK_SECOND) {
+		alarm_sec += timeptr->tm_sec;
+	}
+	if (mask & RTC_ALARM_TIME_MASK_MINUTE) {
+		alarm_sec += (SECS_PER_MIN * timeptr->tm_min);
+	}
+	if (mask & RTC_ALARM_TIME_MASK_HOUR) {
+		alarm_sec += (SECS_PER_HOUR * timeptr->tm_hour);
+	}
+	if (mask & RTC_ALARM_TIME_MASK_WEEKDAY) {
+		alarm_sec += (SECS_PER_DAY * timeptr->tm_wday);
 	}
 
-	while (MXC_RTC_DisableInt(MXC_RTC_INT_EN_LONG) == E_BUSY) {
+	if (alarm_sec > MAX_ALARM_SEC) {
+		return -EINVAL;
 	}
 
-	MXC_RTC_SetTimeofdayAlarm(alarm_sec);
-
-	while (MXC_RTC_EnableInt(MXC_RTC_INT_EN_LONG) == E_BUSY) {
-	}
+	data->mask = mask;
+	data->alarm_sec = alarm_sec;
 
 	return 0;
 }
@@ -185,9 +261,9 @@ static int api_alarm_set_time(const struct device *dev, uint16_t id, uint16_t ma
 static int api_alarm_get_time(const struct device *dev, uint16_t id, uint16_t *mask,
 			      struct rtc_time *timeptr)
 {
-	struct max32_rtc_data *const dev_data = dev->data;
+	struct max32_rtc_data *const data = dev->data;
 
-	if (dev_data->alarms_count <= id) {
+	if (data->alarms_count <= id) {
 		return -EINVAL;
 	}
 
@@ -195,77 +271,170 @@ static int api_alarm_get_time(const struct device *dev, uint16_t id, uint16_t *m
 		return -EINVAL;
 	}
 
-	*timeptr = dev_data->alarm_time;
-	*mask = dev_data->mask;
+	convert_to_rtc_time(data->alarm_sec, 0, timeptr);
+	*mask = data->mask;
 
 	return 0;
 }
 
 static int api_alarm_is_pending(const struct device *dev, uint16_t id)
 {
-	struct max32_rtc_data *const dev_data = dev->data;
+	struct max32_rtc_data *const data = dev->data;
 	int ret;
 
-	if (dev_data->alarms_count <= id) {
+	if (data->alarms_count <= id) {
 		return -EINVAL;
 	}
 
-	ret = (dev_data->alarm_pending == true) ? 1 : 0;
-	dev_data->alarm_pending = false;
+	ret = (data->alarm_pending == true) ? 1 : 0;
+	data->alarm_pending = false;
 
 	return ret;
 }
 
-static void rtc_max32_isr(const struct device *dev)
+static int api_alarm_set_callback(const struct device *dev, uint16_t id,
+				  rtc_alarm_callback callback, void *user_data)
 {
-	struct max32_rtc_data *const dev_data = dev->data;
-	int flags;
+	struct max32_rtc_data *const data = dev->data;
 
-	flags = MXC_RTC_GetFlags();
-
-	if (dev_data->cb) {
-		dev_data->cb(dev, 0, dev_data->cb_data);
+	if (data->alarms_count <= id) {
+		return -EINVAL;
 	}
 
-	dev_data->alarm_pending = false;
-	MXC_RTC_ClearFlags(flags);
+	if (callback == NULL) {
+		data->alarm_cb = NULL;
+		data->alarm_cb_data = NULL;
+		return 0;
+	}
+
+	data->alarm_cb = callback;
+	data->alarm_cb_data = user_data;
+
+	return 0;
 }
-#endif
+#endif /* CONFIG_RTC_ALARM */
+
+#ifdef CONFIG_RTC_UPDATE
+static int api_update_set_callback(const struct device *dev, rtc_update_callback callback,
+				   void *user_data)
+{
+	struct max32_rtc_data *const data = dev->data;
+	const uint32_t sub_sec_alarm_max = 0xFFFFFFFF;
+
+	while (MXC_RTC_SetSubsecondAlarm(sub_sec_alarm_max - SUBSEC_PER_SEC) == E_BUSY) {
+		;
+	}
+
+	if (callback == NULL) {
+		while (MXC_RTC_DisableInt(MXC_RTC_INT_EN_SHORT) == E_BUSY) {
+			;
+		}
+		data->update_cb = NULL;
+		data->update_cb_data = NULL;
+		return 0;
+	}
+
+	data->update_cb = callback;
+	data->update_cb_data = user_data;
+	while (MXC_RTC_EnableInt(MXC_RTC_INT_EN_SHORT) == E_BUSY) {
+		;
+	}
+
+	return 0;
+}
+#endif /* CONFIG_RTC_UPDATE */
+
+#ifdef CONFIG_RTC_CALIBRATION
+static int api_set_calibration(const struct device *dev, int32_t calibration)
+{
+	ARG_UNUSED(dev);
+	if ((calibration > MAX_PPB) || (calibration < MIN_PPB)) {
+		return -EINVAL;
+	}
+
+	while (MXC_RTC_Trim(calibration) == E_BUSY) {
+		;
+	}
+
+	return 0;
+}
+
+static int api_get_calibration(const struct device *dev, int32_t *calibration)
+{
+	ARG_UNUSED(dev);
+
+	*calibration = (int8_t)((uint8_t)(((mxc_rtc_regs_t *)MXC_RTC)->trim) & MXC_F_RTC_TRIM_TRIM);
+
+	return 0;
+}
+#endif /* CONFIG_RTC_CALIBRATION */
+
+static void rtc_max32_isr(const struct device *dev)
+{
+	struct max32_rtc_data *const data = dev->data;
+	int flags = MXC_RTC_GetFlags();
+
+#ifdef CONFIG_RTC_ALARM
+	if (flags & MXC_RTC_INT_FL_LONG) {
+		if (data->alarm_cb) {
+			data->alarm_cb(dev, 0, data->alarm_cb_data);
+			data->alarm_pending = false;
+		} else {
+			data->alarm_pending = true;
+		}
+		MXC_RTC_ClearFlags(MXC_RTC_INT_FL_LONG);
+	}
+#endif /* CONFIG_RTC_ALARM */
+
+#ifdef CONFIG_RTC_UPDATE
+	if (flags & MXC_RTC_INT_FL_SHORT) {
+		if (data->update_cb) {
+			data->update_cb(dev, data->update_cb_data);
+		}
+		MXC_RTC_ClearFlags(MXC_RTC_INT_FL_SHORT);
+	}
+#endif /* CONFIG_RTC_UPDATE */
+}
 
 struct rtc_driver_api rtc_max32_driver_api = {
 	.set_time = api_set_time,
 	.get_time = api_get_time,
-#if defined(CONFIG_RTC_ALARM)
+#ifdef CONFIG_RTC_ALARM
 	.alarm_get_supported_fields = api_alarm_get_supported_fields,
 	.alarm_set_time = api_alarm_set_time,
 	.alarm_get_time = api_alarm_get_time,
 	.alarm_is_pending = api_alarm_is_pending,
+	.alarm_set_callback = api_alarm_set_callback,
 #endif /* CONFIG_RTC_ALARM */
+#ifdef CONFIG_RTC_UPDATE
+	.update_set_callback = api_update_set_callback,
+#endif /* CONFIG_RTC_UPDATE */
+#ifdef CONFIG_RTC_CALIBRATION
+	.set_calibration = api_set_calibration,
+	.get_calibration = api_get_calibration,
+#endif /* CONFIG_RTC_CALIBRATION */
 };
 
 static int rtc_max32_init(const struct device *dev)
 {
-	while (MXC_RTC_Start() == E_BUSY) {
-		;
-	}
-
-#ifdef CONFIG_RTC_ALARM
-	RTC_CFG(dev)->irq_func();
-#endif
+#if defined(CONFIG_RTC_ALARM) || defined(CONFIG_RTC_UPDATE)
+	const struct max32_rtc_config *cfg = dev->config;
+	cfg->irq_func();
+#endif /* CONFIG_RTC_ALARM || CONFIG_RTC_UPDATE */
 	return 0;
 }
 
 #define RTC_MAX32_INIT(_num)                                                                       \
-	IF_ENABLED(CONFIG_RTC_ALARM, (static void max32_rtc_irq_init_##_num(void) {                \
-			   IF_ENABLED(                                                             \
-				   CONFIG_RTC_ALARM,                                               \
-				   (IRQ_CONNECT(DT_INST_IRQN(_num), DT_INST_IRQ(_num, priority),   \
-						rtc_max32_isr, DEVICE_DT_INST_GET(_num), 0);       \
-				    irq_enable(DT_INST_IRQN(_num))));                              \
-		   }));                                                                            \
+	static void max32_rtc_irq_init_##_num(void)                                                \
+	{                                                                                          \
+		IRQ_CONNECT(DT_INST_IRQN(_num), DT_INST_IRQ(_num, priority), rtc_max32_isr,        \
+			    DEVICE_DT_INST_GET(_num), 0);                                          \
+		irq_enable(DT_INST_IRQN(_num));                                                    \
+	};                                                                                         \
 	static const struct max32_rtc_config rtc_max32_config_##_num = {                           \
 		.regs = (mxc_rtc_regs_t *)DT_INST_REG_ADDR(_num),                                  \
-		IF_ENABLED(CONFIG_RTC_ALARM, (.irq_func = max32_rtc_irq_init_##_num, ))};          \
+		.irq_func = max32_rtc_irq_init_##_num,                                             \
+	};                                                                                         \
 	static struct max32_rtc_data rtc_data_##_num = {                                           \
 		.alarms_count = DT_INST_PROP(_num, alarms_count),                                  \
 		.mask = 0,                                                                         \
