ed199003f91d8f4d30147fcab9cfc0ed3f012fd4
Sadik Ozer <sadik.ozer@analog.com>
2023-11-01 17:25:01 +0300

    Review and update TRNG
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>

--- drivers/entropy/entropy_max32.c
+++ drivers/entropy/entropy_max32.c
@@ -4,32 +4,60 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <string.h>
 #include "zephyr/sys/util.h"
 
 #define DT_DRV_COMPAT adi_max32_trng
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/entropy.h>
-#include <errno.h>
-#include <zephyr/init.h>
-#include <zephyr/kernel.h>
-#include <soc.h>
+#include <zephyr/drivers/clock_control/adi_max32_clock_control.h>
 
-#include "trng.h"
+#include <wrap_max32_trng.h>
 
-struct max32_trng_cfg {
+struct max32_trng_config {
 	mxc_trng_regs_t *regs;
+	const struct device *clock;
+	struct max32_perclk perclk;
 };
 
-static int api_get_entropy(const struct device *dev, uint8_t *buffer, uint16_t length)
+static int api_get_entropy(const struct device *dev, uint8_t *buf, uint16_t len)
 {
-	return MXC_TRNG_Random(buffer, length);
+	return MXC_TRNG_Random(buf, len);
 }
 
-static int api_get_entropy_isr(const struct device *dev, uint8_t *buffer, uint16_t length,
-			       uint32_t flags)
+static int api_get_entropy_isr(const struct device *dev, uint8_t *buf, uint16_t len, uint32_t flags)
 {
-	return 0;
+	int ret = 0;
+
+	if ((flags & ENTROPY_BUSYWAIT) == 0) {
+		uint32_t temp;
+		int copy_len;
+		int count = 0;
+
+		while (len) {
+			ret = Wrap_MXC_TRNG_RandomInt_NonBlocking(&temp);
+			if (ret != 0) {
+				break; /* Data not ready do not wait */
+			}
+
+			copy_len = (len > 4) ? 4 : len;
+			memcpy(buf, (uint8_t *)&temp, copy_len);
+
+			len -= copy_len;
+			buf += copy_len;
+			count += copy_len;
+		}
+		ret = count;
+	} else {
+		/* Allowed to busy-wait */
+		ret = api_get_entropy(dev, buf, len);
+		if (ret == 0) {
+			ret = len; /* Data retrieved successfully. */
+		}
+	}
+
+	return ret;
 }
 
 static const struct entropy_driver_api entropy_max32_api = {.get_entropy = api_get_entropy,
@@ -37,11 +65,23 @@ static const struct entropy_driver_api entropy_max32_api = {.get_entropy = api_g
 
 static int entropy_max32_init(const struct device *dev)
 {
-	return MXC_TRNG_Init();
+	int ret = 0;
+	const struct max32_trng_config *cfg = dev->config;
+
+	/* Enable clock */
+	ret = clock_control_on(cfg->clock, (clock_control_subsys_t)&cfg->perclk);
+	if (ret) {
+		return ret;
+	}
+
+	return ret;
 }
 
-static const struct max32_trng_cfg max32_trng_cfg = {
+static const struct max32_trng_config max32_trng_cfg = {
 	.regs = (mxc_trng_regs_t *)DT_INST_REG_ADDR(0),
+	.clock = DEVICE_DT_GET(DT_INST_CLOCKS_CTLR(0)),
+	.perclk.bus = DT_INST_CLOCKS_CELL(0, offset),
+	.perclk.bit = DT_INST_CLOCKS_CELL(0, bit),
 };
 
 DEVICE_DT_INST_DEFINE(0, entropy_max32_init, NULL, NULL, &max32_trng_cfg, PRE_KERNEL_1,
